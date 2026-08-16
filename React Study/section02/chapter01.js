5a33c8a71da7d7f3bca2feeff9f098525a21068b
yeonyhan <jyhan780@gmail.com>
2026-08-16 23:54:50 +0900

    react study

--- React Study/section02/chapter01.js
+++ React Study/section02/chapter01.js
@@ -1,6 +1,9 @@
 // Truthy 와 Falsy
 // 자바스크립트에서는 참, 거짓이 아닌 값도 참, 거짓으로 평가
 
+// 자바스크립트이 모든 값은 Truthy 하거나 Falsy 함
+// 이를 이용하면 조건문을 간결하게 만들 수 있음
+
 // Truthy한 값 (참 같은 값)
 if (123) {
   console.log("123 is true");
@@ -16,8 +19,6 @@ if (undefined) {
 } // undefined is false
 
 // --------------------------------------------
-// 자바스크립트이 모든 값은 Truthy 하거나 Falsy 함
-// 이를 이용하면 조건문을 간결하게 만들 수 있음
 
 // 1. Falsy한 값
 let f1 = undefined;
@@ -26,7 +27,7 @@ let f3 = 0;
 let f4 = -0;
 let f5 = NaN;
 let f6 = ""; // 빈문자열
-let f7 = 0n; // BigInt로 아주 큰 값을 저장할 때 사용
+let f7 = 0n; // BigInt로 아주 큰 값을 저장할 때 사용 (잘 사용하지 않음)
 
 // falsy에 not이 만나 true가 됨
 if (!f1) {
@@ -56,12 +57,12 @@ function printName(person) {
 }
 
 let person; // person 값이 undefined인 경우
-printName(person); // 오류 발생
+// printName(person); // 오류 발생
 
 // 따라서 객체의 프로퍼티에 접근하는 함수의 경우
-// 보통은 조건문으로 매개변수 값이 null 이거나 undefined이 아님을 먼저 확인
+// 조건문으로 매개변수 값이 null 이거나 undefined이 아님을 먼저 확인해야 함
 
-// Truthy, Falsy를 이용하지 않고 진행
+// Truthy, Falsy를 이용하지 않고 진행한다면
 function printName2(person) {
   // 객체가 undefine 또는 null인 경우 함수 종료
   if (person === undefined || person === null) {
@@ -73,12 +74,12 @@ function printName2(person) {
   console.log(person.name);
 }
 
-printName2(person); // person의 값이 없음
+printName2(person); // person의 값이 undefined
 
-// 복잡한 조건문을 반복적으로 적기 떄문에 비효율적
+// 복잡한 조건문을 반복적으로 적는 것은 비효율적
 // Truthy, Falsy를 이용하여 개선
 function printName3(person) {
-  // person 값이 undefined 또는 null로 들어오면
+  // person 값이 Falsy인 경우 (undefined, null)
   // not에 의해 조건식이 true로 평가되어 코드가 실행됨
   if (!person) {
     console.log("person 값이 없음");
@@ -89,5 +90,5 @@ function printName3(person) {
 }
 
 printName3(person); // person의 값이 없음
-let person = { name: "한주연" };
+person = { name: "한주연" };
 printName3(person); // 한주연
--- React Study/section02/chapter02.js
+++ React Study/section02/chapter02.js
@@ -1,85 +1,67 @@
 // 단락평가(Short-circuit Evaluation)
-// AND, OR 같은 논리 연산식에서
+// 논리 연산식(AND, OR)에서
 // 첫번째 피연산자의 값만으로도 연산 결과를 확정할 수 있음
 
 let varA = false;
 let varB = true;
 
-// varA 값만으로도 연산의 결과 확정이 되므로
-// varB 값에는 접근할 필요가 없음
+// 첫번째 피연산자 값만으로도 연산의 결과가 확정되므로
+// 두번째 피연산자 값에는 접근할 필요가 없음
 
 // AND 연산자
-// varA가 F이기 때문에 varB가 무엇이 되었든
+// varA가 F이기 때문에 varB에 상관없이
 // 무조건 결과는 F
-console.log(varA && varB); // F
+console.log(varA && varB); // false
 
 // OR 연산자
-// varB가 T이기 때문에 varA가 무엇이 되었듬
+// varB가 T이기 때문에 varA에 상관없이
 // 무조건 결과는 T
-console.log(varB || varA); // T
+console.log(varB || varA); // true
 
 // 단락평가를 이용하면 조건문을 사용하지 않아도
 // 어떠한 함수를 호출하지 않도록 방지하거나
-// 어떤 값을 굳이 계산하지도록 제한하는 등이 가능해짐
+// 어떤 값을 굳이 계산하지도록 제한하는 등이 가능
 
 // --------------------------------------------
 
 // 간단한 논리연산식
 function returnFalse() {
   console.log("False 함수");
-
-  //단락평가는 Truthy, Falsy 값에도 적용됨
-  return undefined;
+  return false;
 }
 
 function returnTruth() {
   console.log("True 함수");
-  return 10;
+  return true;
 }
 
-// 단락평가가 작동하여 첫번째 피연산자에 의해 결과 확정
+// 첫번째 피연산자만으로 결과 확정된다면 단락평가가 작동
 // 두번째 피연산자는 아예 호출되지 않음
-// Truthy, Falsy은 해당 값 자체가 반환됨
-console.log(returnFalse() && returnTrue()); // False 함수 undefined
+console.log(returnFalse() && returnTrue()); // False 함수 false(연산 결과)
 
 // 단락평가가 작동되지 않음
-// 두번째 피연산자까지 호출하게 됨
-console.log(returnTruth() && returnFalse()); // True 함수 False 함수 undefined
+// 두번째 피연산자까지 호출하게 되어 두 결과 값이 모두 출력
+console.log(returnTruth() && returnFalse()); // True 함수 False 함수 false(연산 결과)
 
 // 단락평가가 작동
-console.log(returnTruth() || returnFalse()); // True 함수 10
+console.log(returnTruth() || returnFalse()); // True 함수 true(연산 결과)
 
 // --------------------------------------------
 
 // 단락평가 활용 사례
-function printName4(person2) {
-  if (!person2) {
-    console.log("person2에 값이 없음");
-    return;
-  }
-  console.log(person2.name);
-}
-
-let person2;
-
-// 단락평가를 이용하여 간결하게 개선
-// person2 값이 Falsy한 값이면 두번째 피연산자 출력
-function printName5(person2) {
-  console.log(person2 && person2.name);
-}
-
-// 한번더 개선
-// 매개변수가 undefined인 경우 (person이 Falsy인 경우)
-// AND 연산자에서 name이 undefined로 평가 (첫번째 피연산자)
-// OR 연산자에서 name의 값이 무시되고 두번째 피연산자 값만 호출
-
-// 매개변수가 Truthy한 경우 (name이 Truthy인 경우)
-// AND 연산자에서 name이 값이 할당됨 (두번째 피연산자)
-// OR 연산자에서 name이 표시됨 (첫번째 피연산자)
-function printName6(person2) {
+// person 값이 Falsy한 값이면 두번째 피연산자 출력
+function printName(person) {
   const name = person && person.name;
   console.log(name || "person의 값이 없음");
 }
 
-printName6(); // undefined
+printName(); // undefined
 printName6({ name: "한주연" }); // 한주연
+
+// person이 Falsy인 경우
+// AND 연산자에서 pesron은 Falsy 이므로 name은 Falsy
+// OR 연산자에서 name의 값이 무시되고 두번째 피연산자 값 호출
+
+// name이 Truthy인 경우
+// AND 연산자에서 person이 Truthy 이고 name에 값이 할당됨
+// OR 연산자에서 name 값을 표시
