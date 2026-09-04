71bfa43dac46886a45e1c80eb68979406bb56fa3
yeonyhan <jyhan780@gmail.com>
2026-09-04 21:40:59 +0900

    react study

--- React Study/section03/src/index.js
+++ React Study/section03/src/index.js
@@ -1,13 +1,6 @@
-// common.js 모듈 시스템의 내장객체 require 이용
-// 모듈의 경로를 인수로 전달
-// exports 한 값을 그대로 반환
+// 1. Common JS 사용하여 함수 사용하기
 // const moduleData = require("./math");
-
 // console.log(moduleData); // add 함수, sub 함수
-
-// // 불러온 객체를 호출하여 사용
-// console.log(moduleData.add(1, 2)); // 3
-// console.log(moduleData.sub(2, 1)); // 1
 
 // // 구조분해 할당 사용시
 // const { add, sub } = require("./math");
@@ -17,23 +10,25 @@
 
 // --------------------------------------------
 
-// ES module을 사용
-// import 와 from을 사용하여 모듈 불러오기
+// 2. ES module 사용하여 함수 사용하기
 // 확장자를 안쓸 경우 오류 발생
 import { add, sub } from "./math.js";
 
 console.log(add(1, 2)); // 3
 console.log(sub(2, 1)); // 1
 
-// 디폴트 모듈을 불러올때
-// 중괄호(구조분해할당) 없이 불러와야함
+// 디폴트 모듈을 불러오는 경우
+// 구조분해할당없이 불러오기
 import multiply from "./math.js";
 
-// 이름을 임의로 바꾸는 것도 가능
-// 어짜피 불러오는 건 기본값으로 정해져 있음
+console.log(multiply(1, 2)); // 2
+
+// 이름을 임의로 수정 가능
 import mul from "./math.js";
 
-// 동일한 경로면 합치는 것도 가능
+console.log(mul(1, 2)); // 2
+
+// 동일한 경로면 합쳐서 불러오기 가능
 import mul, { add, sub } from "./math.js";
 
 // --------------------------------------------
--- React Study/section03/src/math.js
+++ React Study/section03/src/math.js
@@ -1,4 +1,4 @@
-// 계산 기능을 하는 math 모듈
+// 계산 모듈
 
 function add(a, b) {
   return a + b;
@@ -8,11 +8,7 @@ function sub(a, b) {
   return a - b;
 }
 
-// 모듈을 내보내서 다른 모듈에서 불러와 사용해야 함
-// 자바스크립트의 모듈인 common.js 모듈 시스템을 이용
-// 내장객체 exports 라는 프로퍼티의 값으로 객체를 내보내기
-// 객체 안에 각각 프로퍼티로 내보내고 싶은 값을 입력
-
+// 1. Common JS 사용하여 함수 내보내기
 // module.exports = {
 //   add,
 //   sub,
@@ -20,7 +16,7 @@ function sub(a, b) {
 
 // --------------------------------------------
 
-// ES module 사용하여 함수 내보내기
+// 2. ES module 사용하여 함수 내보내기
 export { add, sub };
 
 // 또는 함수 앞에 export 사용
@@ -28,7 +24,7 @@ export function add1(a, b) {
   return a + b;
 }
 
-// 하나의 모듈을 대표하는 디폴트 값을 내보내는 방법
+// 하나의 모듈을 대표하는 디폴트 함수 (default)
 export default function multiply(a, b) {
   return a * b;
 }
